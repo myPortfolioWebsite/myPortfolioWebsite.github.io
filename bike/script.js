3fad070d1019bdc47a0f21e5734bbc9f73eef319
elkorgio87 <58304636+elkorgio87@users.noreply.github.com>
2020-03-26 23:50:36 +0200

    bike

--- bike/script.js
+++ bike/script.js
@@ -1,16 +1,24 @@
-function ibg(){
-    let ibg=document.querySelectorAll(".ibg");
-    for (var i = 0; i < ibg.length; i++) {
-        if(ibg[i].querySelector('img')){
-        ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-        }
-    }
-}
-ibg();
+// function ibg(){
+//     let ibg=document.querySelectorAll(".ibg");
+//     for (var i = 0; i < ibg.length; i++) {
+//         if(ibg[i].querySelector('img')){
+//         ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+//         }
+//     }
+// }
+// ibg();
 
-// $('.wrapper').addClass('loaded');
- 
-$(document).ready(function(){
+
+$(document).ready(function(){function ibg(){
+
+    $.each($('.ibg'), function(index, val) {
+    if($(this).find('img').length>0){
+    $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+    }
+    });
+    }
+    
+    ibg();
     $('.icon-menu').click(function(event) {
         $(this).toggleClass('active');
         $('.menu__body').toggleClass('active');
